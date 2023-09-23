685e40441019efb1cbbe6ab886bdea1041bb0975
Afrizal F.A <77115748+afrzlfa@users.noreply.github.com>
2023-09-23 23:35:51 +0700

    Update Controllers.php

--- app/core/Controllers.php
+++ app/core/Controllers.php
@@ -6,14 +6,14 @@ class Controller
     public function view($view, $data = [])
     {
 
-        require_once '../app/views/' . $view . '.php';
+        require_once('../app/views/' . $view . '.php');
 
     }
 
     public function model($model)
     {
 
-        require_once '../app/models/' . $model . '.php';
+        require_once('../app/models/' . $model . '.php');
         return new $model;
 
     }
