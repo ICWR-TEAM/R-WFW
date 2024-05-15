9cbf117d0cee91cf277c121b41305a8ea1764932
ICWR <icwr@RushOS>
2024-05-15 23:56:02 +0700

    Update

--- app/config/Routes.php
+++ app/config/Routes.php
@@ -14,5 +14,6 @@ $route->Route('get', '/test', "Test::index");
 $route->Route('get', '/testparameter/{1}/testagain/{2}/', "TestParameter::index");
 $route->Route('get', '/testdb', "TestDB::index");
 $route->Route('post', '/test', "Test::post");
+$route->Route('post', '/test/post', "Test::post");
 
 $route->handleRoute();
