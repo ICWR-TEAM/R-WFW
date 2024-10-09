2063dacac574956dbec63fc959837008bdd3d065
Afrizal F.A <77115748+afrzlfa@users.noreply.github.com>
2024-10-09 17:10:36 +0700

    Update Routes.php

--- app/config/Routes.php
+++ app/config/Routes.php
@@ -16,6 +16,7 @@ $route->Route('get', '/testdb', "TestDB::index");
 $route->Route('post', '/test', "Test::post");
 $route->Route('post', '/test/post', "Test::post");
 $route->Route('post', '/test/auth/login', "AuthTest::login");
+$route->Route('post', '/test/auth/check', "AuthTest::check");
 
 // System
 
