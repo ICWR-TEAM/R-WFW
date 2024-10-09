67089acd10e0bfa4e7bc00ebb68a8f17c8163993
afrzlfa <afrzlf4@gmail.com>
2024-10-09 19:16:19 +0700

    Update

--- app/config/Routes.php
+++ app/config/Routes.php
@@ -8,18 +8,18 @@ use App\Core\Router;
 
 $route = new Router();
 
-$route->Route('get', '/', "Home::index");
-$route->Route('get', '/about', "About::index");
-$route->Route('get', '/test', "Test::index");
-$route->Route('get', '/testparameter/{1}/testagain/{2}/', "TestParameter::index");
-$route->Route('get', '/testdb', "TestDB::index");
-$route->Route('post', '/test', "Test::post");
-$route->Route('post', '/test/post', "Test::post");
-$route->Route('post', '/test/auth/login', "AuthTest::login");
-$route->Route('post', '/test/auth/check', "AuthTest::check");
+$route->Route(method: 'get', url: '/', handler: "Home::index");
+$route->Route(method: 'get', url: '/about', handler: "About::index");
+$route->Route(method: 'get', url: '/test', handler: "Test::index");
+$route->Route(method: 'get', url: '/testparameter/{1}/testagain/{2}/', handler: "TestParameter::index");
+$route->Route(method: 'get', url: '/testdb', handler: "TestDB::index");
+$route->Route(method: 'post', url: '/test', handler: "Test::post");
+$route->Route(method: 'post', url: '/test/post', handler: "Test::post");
+$route->Route(method: 'post', url: '/test/auth/login', handler: "AuthTest::login");
+$route->Route(method: 'post', url: '/test/auth/check', handler: "AuthTest::check");
 
 // System
 
-$route->Route('get', '/updater', "Update::install");
+$route->Route(method: 'get', url: '/updater', handler: "Update::install");
 
 $route->handleRoute();
