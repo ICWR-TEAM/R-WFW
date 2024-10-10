cda3cc918e253268d340ac9f40f243097d9341b7
Afrizal F.A <77115748+afrzlfa@users.noreply.github.com>
2024-10-10 08:53:07 +0700

    Update About.php

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -12,9 +12,9 @@ class About extends Controller
 
         $data['title'] = 'About Us - incrustwerush.org';
 
-        $this->view('templates/header', $data);
-        $this->view('home/about', $data);
-        $this->view('templates/footer', $data);
+        $this->view(view: 'templates/header', data: $data);
+        $this->view(view: 'home/about', data: $data);
+        $this->view(view: 'templates/footer', data: $data);
 
     }
 
