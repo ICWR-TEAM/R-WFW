63533870ca0a3f0755f5c4ba267ec8f472033541
Afrizal F.A <77115748+afrzlfa@users.noreply.github.com>
2024-10-20 14:33:49 +0700

    Update ErrorHandling.php

--- app/core/ErrorHandling.php
+++ app/core/ErrorHandling.php
@@ -13,14 +13,14 @@ class ErrorHandling
     public function customError(string $errno, string $errstr, string $errfile, int $errline): void
     {
         $errorMessage = "<b>Error:</b> [$errno] $errstr <br> in <b>$errfile</b> on line <b>$errline</b>";
-        error_log(message: "Error: [$errno] $errstr in $errfile on line $errline\n", message_type: 3, destination: '../tmp/' . date(format: "Y-m-d_H:i:s") . '_errors.log');
+        error_log(message: "Error: [$errno] $errstr in $errfile on line $errline\n", message_type: 3, destination: '../tmp/' . date(format: "Y-m-d") . '_errors.log');
         echo $this->formatError(message: $errorMessage);
     }
 
     public function customException(\Throwable $exception): void
     {
         $errorMessage = "<b>Uncaught exception:</b> " . $exception->getMessage() . "<br> in <b>" . $exception->getFile() . "</b> on line <b>" . $exception->getLine() . "</b>";
-        error_log(message: "Uncaught exception: " . $exception->getMessage() . " in " . $exception->getFile() . " on line " . $exception->getLine() . "\n", message_type: 3, destination: '../tmp/' . date(format: "Y-m-d_H:i:s") . '_errors.log');
+        error_log(message: "Uncaught exception: " . $exception->getMessage() . " in " . $exception->getFile() . " on line " . $exception->getLine() . "\n", message_type: 3, destination: '../tmp/' . date(format: "Y-m-d") . '_errors.log');
         echo $this->formatError(message: $errorMessage);
     }
 
