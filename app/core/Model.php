6fd8ad5befddd280420c91758fd3da516c9339ba
Afrizal F.A <77115748+afrzlfa@users.noreply.github.com>
2024-10-31 14:46:45 +0700

    Update Model.php

--- app/core/Model.php
+++ app/core/Model.php
@@ -4,12 +4,14 @@ namespace App\Core;
 
 use App\Core\Updater;
 use App\Core\Database;
+use App\Core\Response;
 use App\Middleware\Auth;
 
 class Model
 {
     public $auth;
     public $db;
+    public $response;
 
     public function __construct()
     {
@@ -21,6 +23,7 @@ class Model
             $this->db = new Database();
         }
 
+        $this->response = new Response();
         $this->auth = new Auth(secretKey: SECRET_KEY_JWT, privateKeyPath: SIGNATURE_PRIVATE_KEY, publicKeyPath: SIGNATURE_PUBLIC_KEY);
     }
 }
