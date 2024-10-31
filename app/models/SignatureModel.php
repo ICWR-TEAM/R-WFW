29bd90318363bdb2a1e960a647b14b31ee303eeb
Afrizal F.A <77115748+afrzlfa@users.noreply.github.com>
2024-10-31 14:34:35 +0700

    Update SignatureModel.php

--- app/models/SignatureModel.php
+++ app/models/SignatureModel.php
@@ -13,7 +13,6 @@ class SignatureModel extends Model
         if ($input = file_get_contents(filename: "php://input")) {
 
             if (json_decode(json: $input) !== null && json_last_error() === JSON_ERROR_NONE) {
-                
                 $signature = $this->auth->generateSignature(data: $input);
 
                 return json_encode(value: [
@@ -50,6 +49,79 @@ class SignatureModel extends Model
             return json_encode(value: [
                 "status" => "500",
                 "message" => "error"
+            ]);
+        }
+    }
+
+    public function SignSpecific(): bool|string
+    {
+        if ($input = file_get_contents(filename: "php://input")) {
+
+            if (json_decode(json: $input) !== null && json_last_error() === JSON_ERROR_NONE) {
+                $input_array = json_decode(json: $input, associative: true);
+                $data = [
+                    "amount" => $input_array['amount'],
+                    "qty" => $input_array['qty']
+                ];
+
+                $json_generate = json_encode(value: $data);
+                $this->auth->generateSignature(data: $json_generate);
+
+                return json_encode(value: [
+                    "status" => "200",
+                    "message" => "success",
+                    "data" => $data
+                ]);
+            } else {
+                return json_encode(value: [
+                    "status" => "401",
+                    "message" => "Invalid JSON Payload!"
+                ]);
+            }
+        } else {
+            return json_encode(value: [
+                "status" => "500",
+                "message" => "error"
+            ]);
+        }
+    }
+
+    public function SignSpecificCheck(): bool|string
+    {
+
+        if ($input = file_get_contents(filename: "php://input")) {
+
+            if (json_decode(json: $input, associative: true) !== null && json_last_error() === JSON_ERROR_NONE) {
+                $input_decode = json_decode(json: $input, associative: true);
+                $receivedSignature = $_SERVER['HTTP_X_SIGNATURE'] ?? '';
+
+                $input_data = [
+                    "amount" => $input_decode['data']['amount'],
+                    "qty" => $input_decode['data']['qty']
+                ];
+
+                $data = json_encode(value: $input_data);
+
+                if ($this->auth->verifySignature(data: $data, signature: $receivedSignature)) {
+                    return json_encode(value: [
+                        "status" => "200",
+                        "message" => "success",
+                        "data" => [
+                            "data" => $input_data
+                        ]
+                    ]);
+                } else {
+                    return json_encode(value: [
+                        "status" => "error",
+                        "code" => 403,
+                        "message" => "Invalid Signature."
+                    ]);
+                }
+            }
+        } else {
+            return json_encode(value: [
+                "status" => "401",
+                "message" => "Invalid JSON Payload!"
             ]);
         }
     }
