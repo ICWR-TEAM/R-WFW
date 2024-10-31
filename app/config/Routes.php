76a974dbde7818facd513f827fae15e2399218ab
Afrizal F.A <77115748+afrzlfa@users.noreply.github.com>
2024-10-31 14:33:30 +0700

    Update Routes.php

--- app/config/Routes.php
+++ app/config/Routes.php
@@ -21,6 +21,8 @@ $route->Route(method: 'post', url: '/test/auth/check', handler: "AuthTest::check
 $route->Route(method: 'post', url: '/test/auth/check_header', handler: "AuthTest::checkHeader");
 $route->Route(method: 'post', url: '/test/sign/create', handler: "Signature::create");
 $route->Route(method: 'post', url: '/test/sign/check', handler: "Signature::check");
+$route->Route(method: 'post', url: '/test/sign/specific/create', handler: "Signature::signSpecific");
+$route->Route(method: 'post', url: '/test/sign/specific/check', handler: "Signature::signSpecificCheck");
 
 // System
 
